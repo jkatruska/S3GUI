29cc31b66d5e5183fe123ca3a173aabed7d77020
jozefkatruska <jkatruska@gmail.com>
2021-11-22 16:12:58 +0100

    added docker file, added git ignore

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("txt")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
